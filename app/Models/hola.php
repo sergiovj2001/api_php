2374000f092d449a32fa5725a104b05873870257
Sergio Vera <a19vejuse@iesgrancapitan.org>
2022-02-02 13:59:09 +0100

    añadiendo estrucutura y variables env

---
+++ app/Models/hola.php
@@ -0,0 +1,9 @@
+<?php
+/**
+*
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+* @author sergio vera jurado<a19vejuse@iesgrancapitan.org>
+*
+*/
+echo "<h1>Hola mundo</h1>";
+?>
---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+*
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+* @author sergio vera jurado<a19vejuse@iesgrancapitan.org>
+*
+*/
+require "../bootstrap.php";
+echo DB_HOST;
+echo DB_USER;
+?>
